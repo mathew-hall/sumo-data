9a6252272355b2c525d5e2440cf92ec2052682e6
Mathew Hall <mathew.james.hall@gmail.com>
2017-02-06 14:01:23 +0000

    Add more analysis & plots for KW tests for RQ2

--- dist/test_mojo.R
+++ dist/test_mojo.R
@@ -1,8 +1,26 @@
+results.75yr <- function(){
+  systematic_results[systematic_results$yesRatio == .75,]
+}
+
+rq3_distinct_mojo_values <- function(){
+  multipleMoJos <- ddply(results.75yr(),
+                         .(MDGmdg),
+                         summarise,
+                         distinctMoJos=length(unique(startMOJO)),
+                         multipleMoJos=length(unique(startMOJO)) > 1)
+
+  metadata <- unique(systematic_results[,c("MDGmdg","Classes","Packages")])
+
+  join(multipleMoJos, metadata, by="MDGmdg")
+}
+
+
 (rq3_kw_tests <- function(){
 
   # First find cases that have more than one MoJo value:
-  results.75yr <- systematic_results[systematic_results$yesRatio == .75,]
-  multipleMoJos <- ddply(results.75yr, .(MDGmdg), summarise, multipleMoJos=length(unique(startMOJO)) > 1)
+  multipleMoJos <- rq3_distinct_mojo_values()
+
+  results.75yr <- results.75yr()
 
   subset <- results.75yr[join(results.75yr, multipleMoJos, by="MDGmdg")$multipleMoJos,]
 
@@ -11,15 +29,38 @@
   p.values <- ddply(subset, .(MDGmdg), function(df){
   	df$totalRels <- df$positiveRelations + df$negativeRelations
   	res <- kruskal.test(formula=totalRels ~ startMOJO, data=df)
-  	return(res$p.value)
+  	res$p.value
   })
 
   p.values$adjusted <- p.adjust(p.values$V1, method="BH")
 
   alpha <- 0.05
 
-  writeLines(paste("Significant results at alpha =, ",alpha, " after adjustment:", sum(p.values$adjusted < alpha)))
+  writeLines(paste("Significant results at alpha = ",alpha, " after adjustment:", sum(p.values$adjusted < alpha)))
 
   p.values$significant <- with(p.values, adjusted < alpha)
-  p.values
+  join(multipleMoJos, p.values, by = 'MDGmdg')
+})()
+
+
+(rq3_signif_histogram <- function(){
+  labels <- c('One MoJo Value', 'Multiple MoJo Values, Insignificant', 'Multiple MoJo Values, Significant')
+  rq3_kw_tests() %>%
+    mutate(
+      fac =
+        factor(ifelse(
+          is.na(significant),
+            labels[1],
+            ifelse(significant,
+                  labels[3],
+                  labels[2])),levels = labels)
+      ) %>%
+      ggplot(aes(Classes)) +
+      geom_histogram(binwidth=10,fill=NA, colour='black') +
+      facet_grid(fac~.,)  +
+      theme_bw() +
+      labs(x="Subject Size (Classes)", y="Subject Count") -> g
+    ggsave(filename = "inst/fig/rq3-mojo-signif.pdf", g, width=7,height=8)
+
+    g
 })()
